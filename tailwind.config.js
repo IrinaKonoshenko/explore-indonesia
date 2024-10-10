805cb2b73ec113104e2f424d0d6c764cf7c97b4b
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2024-10-10 18:18:16 +0500

    add fourth and fifth block

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         "logo-1": "url('./img/logo.svg')",
         "palm-1": "url('./img/4.jpg')",
         "mountain-1": "url('./img/8.jpg')",
+        "waves-1": "url('./img/9.jpeg')",
+        "palm-2": "url('./img/10-2.jpg')",
       },
       colors: {
         gray: "#40424C",
@@ -19,6 +21,7 @@ module.exports = {
         "light-text": "#151439",
         "paragraph-dark": "#6F7787",
         "bg-color": "#020C21",
+        "bg-button": "#E93A7D",
       },
       fontSize: {
         h1: ["120px", "130px"],
@@ -33,6 +36,8 @@ module.exports = {
         "header-container": "1751px",
         "palm-container": "1280px",
         "mountain-container": "1277px",
+        "waves-container": "614px",
+        "palm-container-2": "1275px",
       },
     },
   },
