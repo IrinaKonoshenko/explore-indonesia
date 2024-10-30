e2fe3c9d165679f28246ffb7358c8d74025bed64
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2024-10-31 03:51:04 +0500

    add mobile version

--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ const header = document.getElementById("header");
 document.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
   if (scrollPosition >= 200) {
-    console.log(scrollPosition);
     header.classList.add("newClass");
   } else {
     header.classList.remove("newClass");
   }
 });
+
+const btnUp = {
+  el: document.getElementById("btn"),
+  show() {
+    this.el.classList.remove("btn-up_hide");
+  },
+  hide() {
+    this.el.classList.add("btn-up_hide");
+  },
+  addEventListener() {
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 500 ? this.show() : this.hide();
+    });
+    document.getElementById("btn").onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
